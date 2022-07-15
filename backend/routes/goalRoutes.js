7c3969a49094c26a63bb9ae8334a1bb712024dc5
Kenny Yang <kennyyangh@gmail.com>
2022-07-14 22:33:22 -0700

    User authentication

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -6,21 +6,23 @@ const { getGoals,
         deleteGoal 
 } = require('../controllers/goalController')
 
+const { protect } = require('../middleware/authMiddleware')
+
 // the "/" is at the end of api/goals/
-router.get('/', getGoals)
+// router.get('/', protect, getGoals)
 
-//creating post
-router.post('/', setGoal)
+// //creating post
+// router.post('/', setGoal)
 
-//updating post
-router.put('/:id', updateGoal)
+// //updating post
+// router.put('/:id', updateGoal)
 
-//deleting post
-router.delete('/:id', deleteGoal)
+// //deleting post
+// router.delete('/:id', deleteGoal)
 
 
 //you can clean the code up even more with
-// router.route('/').get(getGoals).post(setGoal)
-// router.route('/:id').delete(deleteGoal).put(updateGoal)
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal)
 
 module.exports = router
